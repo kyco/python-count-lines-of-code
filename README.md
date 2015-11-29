4f43361e186d5e43a1775e6bc2ad5c977b80e9cb
Kyluke McDougall <kyluke@journeyatrest.com>
2015-11-29 14:11:48 +0200

    Updated  - Added the ability to include csv files with excluded file types

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This was written in Python 2.7 and was tested in Linux.
 ##Running
 You can call it by running
 
-	cloc.py /path/of/your/project
+	cloc.py /path/of/your/project /path/to/excludelist.csv
 
 It has an array with the extensions that will be excluded, if you have certain extensions that you need excluded from the count, add them to the array
 
@@ -20,7 +20,7 @@ It has an array with the extensions that will be excluded, if you have certain e
  - If no arguments are found from the command line, ask the user for an input inside of the application.
  - There is a version for Python 3.4+ in a separate branch called python3
  - Has been tested and works in Windows 10. There is a bug where it picks up random file types by searching through what is probably the .git folder.
+ - Include a csv to define excluded file types
 
 ##To Do
- - Give the user the ability to upload a csv to define excluded file types
  - If a .gitignore file is found, ask the user if they would like to use that to append to their current ignore list
--- cloc.py
+++ cloc.py
@@ -1,15 +1,35 @@
 import os
 import sys
 import fnmatch
+import StringIO
+import csv
 
 try:
     path = sys.argv[1]
 except IndexError as e:
     path = input('file path: ')
+try:
+    exclude_file = sys.argv[2]
+except IndexError as e:
+    exclude_file = input('exclude file: ')
 
 extensions = []
 code_count = []
 exclude = ['sample','exclude','description','png','jpg','config','HEAD','packed-refs','idx','master','pack','txt','index','gitignore']
+folder_exclude = ['.git']
+total = 0
+
+try:
+    exclude_file
+except NameError:
+    pass
+else:
+    with open(exclude_file, 'r+') as f:
+        content = f.read().replace('\n', '')
+
+    reader = csv.reader(content.split('\n'), delimiter=',')
+    for row in reader:
+        exclude += row
 
 def file_len(fname):
     i = 0
@@ -18,12 +38,21 @@ def file_len(fname):
             pass
     return i + 2
 
-def get_extensions(path,excl):
-    for root, dir, files in os.walk(path):
+def get_extensions(path,excl,folder_excl):
+    for root, dir, files in os.walk(path): 
             for items in fnmatch.filter(files, "*"):
+
                 temp_extensions = items.rfind(".")
+
+                # Remove any folders that are meant to be excluded from the search
+                for x in folder_excl:
+                    if x in dir:
+                        dir.remove(x)
+                    pass
+
                 ext = items[temp_extensions+1:]
 
+                # If the file is not in the exclude list, add it into the array
                 if ext not in extensions:
                     if ext not in excl:
                         extensions.append(ext)
@@ -37,12 +66,31 @@ def count_per_ext(path,extension):
                 temp += file_len(value)
     code_count.append(temp)
 
-get_extensions(path,exclude)
+get_extensions(path,exclude,folder_exclude)
 
 for run in extensions:
     count_per_ext(path,"*."+run)
     pass
 
+print '\n'
+print '\tLines of code by extension'
+print '\t---------------------------'
+print ''
+print '\tExtension \t\tLine Count'
+print '\t--------- \t\t----------'
+
 for idx, val in enumerate(code_count):
-    print extensions[idx] + ": " + str(code_count[idx])
+    count = code_count[idx]-1
+    total += count
+    if len(extensions[idx]) > 10:
+        t = '\t'
+    elif len(extensions[idx]) > 7:
+        t = '\t\t'
+    else:
+        t = '\t\t\t'
+    print '\t' + extensions[idx] + ": " + t + str(count)
     pass
+
+print '\t\t\t'
+print '\tTotal: \t\t\t' + str(total)
+print '\n'
